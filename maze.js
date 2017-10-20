b759ce4173a17c2fa66896835bd166c3abcefccf
ovophil <philroywilliams@yahoo.com>
2017-10-20 00:57:04 -0500

    Exercise 3

--- maze.js
+++ maze.js
@@ -1,3 +1,5 @@
+var wall = false;
+
 window.onload = function(){
 
 	var begin = document.getElementById("start");
@@ -5,23 +7,35 @@ window.onload = function(){
 	var bound = document.querySelectorAll(".boundary");
 	var state = document.getElementById("status");
 	var game = document.getElementById("maze");
-	var wall = false;
+	
 
 	for(var x = 0; x < bound.length; x++){
 		bound.item(x).addEventListener("mouseover", touchbound);
+		bound.item(x).addEventListener("mouseover", alerts);
 	}
 }
 
 //when mouse touches any boundary it turns red
-var touchbound = function()
-	{
+var touchbound = function(){
+
 		wall = true;
 		var bound = document.querySelectorAll(".boundary");
 		for(var x = 0; x < bound.length; x++)
 		{		
 			bound.item(x).setAttribute("class", "boundary youlose");
-		}		 
+		}	
 	}
+
+var alerts = function(){
+	var state = document.getElementById("status");
+	if(wall === true)
+	{			 
+		state.innerHTML  = "You Lose";
+	}else{
+		state.innerHTML = "You Win......";
+	}
+}
+
 
 	 
 
